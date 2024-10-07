aff2d76ded655740f57d1659f5ba7fe5a082d14f
GabrielAzamfiri <167236687+GabrielAzamfiri@users.noreply.github.com>
2024-10-07 21:10:33 +0200

    add info selected anime

--- src/components/Anime.jsx
+++ src/components/Anime.jsx
@@ -1,16 +1,48 @@
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
+
 import { useSelector } from "react-redux";
 
 const Anime = () => {
-  const animeClicked = useSelector(state => state.animeClick.animeClicked);
+  const selectedAnime = useSelector(state => state.animeClick.animeClicked);
 
   return (
-    <Container>
-      {animeClicked && (
+    <Container fluid>
+      {selectedAnime && (
         <>
-          <img src={animeClicked.image_url} alt={animeClicked.title} />
-          <h1>{animeClicked.title}</h1>
-          <p>{animeClicked.synopsis}</p>
+          <Row>
+            <img src={selectedAnime.images.jpg.large_image_url} alt={selectedAnime.title} style={{ width: "200px" }} />
+            <Col>
+              <h1>{selectedAnime.title}</h1>
+              <p className="fs-7">{selectedAnime.synopsis}</p>
+            </Col>
+          </Row>
+
+          <h5 className="mt-3">Genres:</h5>
+          <ul>
+            {selectedAnime.genres.map(genre => (
+              <li className="fs-7" key={genre.mal_id}>
+                {genre.name}
+              </li>
+            ))}
+          </ul>
+
+          <h5 className="mt-3">Stats:</h5>
+          <p className="fs-7 m-0">Score: {selectedAnime.score}</p>
+          <p className="fs-7 m-0">Ranked: {selectedAnime.rank}</p>
+          <p className="fs-7 m-0">Popularity: {selectedAnime.popularity}</p>
+          <p className="fs-7 m-0">Favorites: {selectedAnime.favorites}</p>
+
+          <h5 className="mt-3">Alternative Titles:</h5>
+          <p className="fs-7 m-0">Title english: {selectedAnime.title_english}</p>
+          <p className="fs-7 m-0">Title japanese: {selectedAnime.title_japanese}</p>
+
+          <h5 className="mt-3">Episodes:</h5>
+          <p className="fs-7 m-0">Episodes: {selectedAnime.episodes}</p>
+          <p className="fs-7 m-0">Aired: {selectedAnime.aired.string}</p>
+          <p className="fs-7 m-0">Duration: {selectedAnime.duration}</p>
+          <p className="fs-7 m-0">Source: {selectedAnime.source}</p>
+          <p className="fs-7 m-0">Rating: {selectedAnime.rating}</p>
+          <p className="fs-7 m-0">Status: {selectedAnime.status}</p>
         </>
       )}
     </Container>
